99a14487c97ede0a49e474288994fa67e3f9677b
abildaev <abildaev@ptsecurity.com>
2025-04-21 19:44:15 +0300

    fix: fixes for erik comments

--- libs/domain/place/src/lib/ui/place-card/place-card.component.ts
+++ libs/domain/place/src/lib/ui/place-card/place-card.component.ts
@@ -1,11 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  HostListener,
-  Input,
-  Output,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Place } from '@libs/api';
 import { RatingComponent } from '@libs/core';
 import { TuiIcon } from '@taiga-ui/core';
@@ -21,12 +14,4 @@ import { TuiIcon } from '@taiga-ui/core';
 export class PlaceCardComponent {
   @Input({ required: true })
   place!: Place;
-
-  @Output()
-  clickCard = new EventEmitter<Place>();
-
-  @HostListener('click', ['$event'])
-  emitClickCard(): void {
-    this.clickCard.emit(this.place);
-  }
 }
